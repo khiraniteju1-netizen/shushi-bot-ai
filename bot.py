eaaea81daa2c8ff0116c2a0c60b4a98bb10e5910
khiraniteju1-netizen <khiraniteju1@gmail.com>
2026-04-09 13:33:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -144,3 +144,26 @@ if __name__ == "__main__":
     while True:
         try: bot.infinity_polling(timeout=20)
         except: time.sleep(5)
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I am alive!"
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+# --- BAaki Aapka Purana Code Yahan Se Shuru Hoga ---
+# ... (Bot ka logic) ...
+
+if __name__ == "__main__":
+    init_db()
+    keep_alive() # Yeh line server start karegi
+    bot.infinity_polling()
